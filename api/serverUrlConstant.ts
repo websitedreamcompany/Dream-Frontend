44918f4d4bc1bb29475c01633b07b94a416f9964
Devjoe <benagu477@gmail.com>
2026-04-30 17:14:33 +0100

    update on code

--- api/serverUrlConstant.ts
+++ api/serverUrlConstant.ts
@@ -1,4 +1,4 @@
 
-export const BASE_URL = "localhost:3070/api/v1" 
+export const BASE_URL = "https://website-wompany-ycfb.onrender.com/api/v1" 
 
 //"https://website-wompany-ycfb.onrender.com/api/v1";
